1d9def5678b9c8ce88de1af8572ecd4a412eb799
jzyong <13250433161aA>
2020-04-16 10:45:31 +0800

    添加了overflow，完善了dispaly

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,30 @@ const { task, src, dest } = require("gulp");
 const css = require("gulp-css");
 const insert = require("gulp-insert");
 const fs = require("fs");
+const autoprefixer = require("gulp-autoprefixer");
 
 /**
  * 获取文件路径
  * @param {String} path 基于当前文件的相对路径，例如src/dispaly.css
  */
-const getFilePath = function(path) {
+const getFilePath = function (path) {
     return __dirname + "/" + path;
 }
 
 
 // 打包
-task("build", function(done) {
+task("build", function (done) {
     src("./src/index.css")
-    .pipe(insert.prepend(
-        fs.readFileSync(getFilePath("src/display.css"))
-    ))
-    .pipe(css())
-    .pipe(dest("./dist"));
+        .pipe(insert.prepend(
+            fs.readFileSync(getFilePath("src/display.css"))
+        ))
+        .pipe(insert.prepend(
+            fs.readFileSync(getFilePath("src/overflow.css"))
+        ))
+        .pipe(autoprefixer({
+            browsers: ["last 2 versions"],
+        }))
+        .pipe(css())
+        .pipe(dest("./dist"));
     done();
 });
